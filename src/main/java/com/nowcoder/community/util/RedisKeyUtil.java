f159f9b8432c4d7f9a3543b6a3dadbb385af3a10
jokerz <2640702247@qq.com>
2023-04-02 15:12:10 +0800

    新增关注功能,优化登录模块

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -5,6 +5,12 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee";
+    private static final String PREFIX_FOLLOWER = "follower";
+    private static final String PREFIX_KAPTCHA = "kaptcha";
+    private static final String PREFIX_TICKET = "ticket";
+
+    private static final String PREFIX_USER = "user";
 
 
     //某个实体的赞
@@ -19,4 +25,29 @@ public class RedisKeyUtil {
         return PREFIX_USER_LIKE + SPLIT + userId;
     }
 
+    //某个用户关注的实体
+    //followee:userId:entityType -> zset(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType){
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    //某个实体拥有的粉丝
+    //follower:entityType:entityId -> zset(userId.now)
+    public static String getFollowerKey(int entityType, int entityId){
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    //登录验证码
+    public static String getKaptchaKey(String owner){
+        return PREFIX_KAPTCHA + SPLIT + owner;
+    }
+
+    //登录的凭证
+    public static String getTicketkey(String ticket){
+        return PREFIX_TICKET + SPLIT + ticket;
+    }
+
+    public static String getUserkey(int userId){
+        return PREFIX_TICKET + SPLIT + userId;
+    }
 }
