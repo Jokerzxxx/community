ae76d281e8133577cd30ed18d84f9068392f3fba
jokerz <2640702247@qq.com>
2023-04-06 11:08:39 +0800

    Redis实现网站数据统计

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -9,8 +9,12 @@ public class RedisKeyUtil {
     private static final String PREFIX_FOLLOWER = "follower";
     private static final String PREFIX_KAPTCHA = "kaptcha";
     private static final String PREFIX_TICKET = "ticket";
-
     private static final String PREFIX_USER = "user";
+
+    private static final String PREFIX_UV = "uv";
+
+    private static final String PREFIX_DAU = "dau";
+
 
 
     //某个实体的赞
@@ -46,8 +50,28 @@ public class RedisKeyUtil {
     public static String getTicketkey(String ticket){
         return PREFIX_TICKET + SPLIT + ticket;
     }
-
+    //用户
     public static String getUserkey(int userId){
         return PREFIX_TICKET + SPLIT + userId;
+    }
+
+    //单日uv
+    public static String getUVKey(String date){
+        return PREFIX_UV + SPLIT + date;
+    }
+
+    //区间uv
+    public static String getUVKey(String startDate, String endDate){
+        return PREFIX_UV + SPLIT + startDate + SPLIT + endDate;
+    }
+
+    //单日活跃用户
+    public static String getDAUKey(String date){
+        return PREFIX_DAU + SPLIT + date;
+    }
+
+    //区间活跃用户
+    public static String getDAUKey(String startDate, String endDate){
+        return PREFIX_DAU + SPLIT + startDate + SPLIT + endDate;
     }
 }
